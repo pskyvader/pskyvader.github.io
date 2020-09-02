4810633097a6d0d4ac066ef67f333febc89ca39c
Pablo <pskyvader@hotmail.com>
2020-09-02 14:52:15 -0500

    sudoku uploaded

---
+++ sudoku/precache-manifest.7fefde501e730a0516a65ee02d9b48f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70fd9c96ec8c53003b92f7d7cb7d103e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "095a0123ba7d73afc314",
+    "url": "/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "4e55d495e7f5ce131b17",
+    "url": "/static/js/2.a1e7b4c0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.a1e7b4c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "095a0123ba7d73afc314",
+    "url": "/static/js/main.381dbcc5.chunk.js"
+  },
+  {
+    "revision": "c71dd58cd5349d4e6ab8",
+    "url": "/static/js/runtime-main.4dd858cb.js"
+  }
+]);
