4e8123ff9b443ed0a4985393fbec8f4e43212c51
Pablo <pskyvader@hotmail.com>
2020-09-22 15:48:00 -0500

    Language support added

--- sudoku/service-worker.js
+++ sudoku/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.77c230e619bcdd95e60792e081d01ef0.js"
+  "./precache-manifest.e35a6a7ac9d158403a21814417b43819.js"
 );
 
 self.addEventListener('message', (event) => {
